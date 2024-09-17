22e98b2d6af24ebda5d74f01789bfab7f90bf74a
Artem <artemkolisnychenko@gmail.com>
2024-09-17 14:33:38 +0300

    Add files via upload

---
+++ Additional Task/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ClickerGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.clicks = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(220, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Таймер:";
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time.Location = new System.Drawing.Point(322, 60);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(0, 21);
+            this.time.TabIndex = 0;
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClick.Location = new System.Drawing.Point(150, 84);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(300, 150);
+            this.buttonClick.TabIndex = 1;
+            this.buttonClick.Text = "CLICK HERE";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            this.buttonClick.Click += new System.EventHandler(this.buttonClick_Click);
+            // 
+            // clicks
+            // 
+            this.clicks.AutoSize = true;
+            this.clicks.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clicks.Location = new System.Drawing.Point(13, 13);
+            this.clicks.Name = "clicks";
+            this.clicks.Size = new System.Drawing.Size(120, 21);
+            this.clicks.TabIndex = 2;
+            this.clicks.Text = "Кількість кліків:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(606, 263);
+            this.Controls.Add(this.clicks);
+            this.Controls.Add(this.buttonClick);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.Label clicks;
+    }
+}
+
---
+++ Additional Task/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClickerGame
+{
+    public partial class MainForm : Form
+    {
+        private Timer gameTimer;
+        private int timeCount;
+        private int clickCount;
+        private bool gameStarted = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonClick.Enabled = true;
+        }
+
+        private void UpdateUI()
+        {
+            time.Text = $"{timeCount}";
+            clicks.Text = $"Кількість кліків: {clickCount}";
+            buttonClick.Enabled = timeCount > 0;
+        }
+
+        private void StartGame()
+        {
+            timeCount = 15;
+            clickCount = 0;
+            UpdateUI();
+
+            gameTimer = new Timer();
+            gameTimer.Interval = 1000;
+            gameTimer.Tick += (sender, e) =>
+            {
+                timeCount--;
+                UpdateUI();
+
+                if (timeCount == 0) 
+                {
+                    EndGame();
+                }
+            };
+
+            gameTimer.Start();
+            buttonClick.Enabled = true;
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            MessageBox.Show($"Час вийшов!\nВаш результат: {clickCount}.", "Гра закінчена");
+            gameStarted = false;
+            buttonClick.Enabled = true;
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted)
+            {
+                gameStarted = true;
+                StartGame();
+            }
+            clickCount++;
+            UpdateUI();
+        }
+    }
+}
